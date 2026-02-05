6b4a2fc6aa32de96bf497882837bde609e0456dc
auraluvsu <ntreasure2015@gmail.com>
2026-02-05 19:39:20 +0000

    messing around with ambiguity

--- src/test.cc
+++ src/test.cc
@@ -1,44 +1,45 @@
 #include <print>
 #include <string>
 
-void qs(char *items, int left, int right) {
-    int i, j;
-    char x, y;
+// void qs(char *items, int left, int right) {
+//     int i, j;
+//     char x, y;
+//
+//     i = left;
+//     j = right;
+//     x = items[(left + right) / 2];
+//
+//     do {
+//         while ((items [i] < x) && (i < right)) i++;
+//         while ((x < items[j]) && (j > left)) j--;
+//
+//         if (i <= j) {
+//             y = items[i];
+//             items[i] = items[j];
+//             items[j] = y;
+//             i++; j--;
+//
+//         }
+//     } while (i <= j);
+//     if (left < j) qs (items, left, j);
+//     if (i < right) qs(items, i, right);
+// }
+//
+// void quickSort(char *items, int len) {
+//     qs(items, 0, len-1);
+// }
 
-    i = left;
-    j = right;
-    x = items[(left + right) / 2];
-
-    do {
-        while ((items [i] < x) && (i < right)) i++;
-        while ((x < items[j]) && (j > left)) j--;
-
-        if (i <= j) {
-            y = items[i];
-            items[i] = items[j];
-            items[j] = y;
-            i++; j--;
-
+namespace std {
+    void foo() {
+        int args[] = {1,2,3,4,5};
+        for (auto i : args) {
+            std::print("{}", i);
         }
-    } while (i <= j);
-    if (left < j) qs (items, left, j);
-    if (i < right) qs(items, i, right);
-}
-
-void quickSort(char *items, int len) {
-    qs(items, 0, len-1);
-}
-
-void my_print(const char* foo) {
-    std::println("Int was called");
-    std::println("{}", foo);
-}
-void my_print(const int foo[]) {
-    std::println("Double was called");
-    std::println("{}", foo);
+        std::print("\n");
+    }
 }
 
 int main() {
-    my_print("Hello");
-    return 0;
+    int foo[] = {1,2,3,4,5};
+    std::foo();
 }
